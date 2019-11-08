b13b1fbd8b5815ba2e28fc8b275ab25316f1df34
Vikintas Raudavicius <vikintas.raudavicius@gmail.com>
2019-11-08 19:43:35 +0000

    Updates

---
+++ precache-manifest.439980b373e9addc04297bc8047156b8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8b6a24b8bf0ef3cd098d2d25161adb2",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0c62aadb28ef57f8bcaf",
+    "url": "/portfolio/static/css/main.9eacecc1.chunk.css"
+  },
+  {
+    "revision": "880c87b0a366b7ded5c0",
+    "url": "/portfolio/static/js/2.7c0c7bb0.chunk.js"
+  },
+  {
+    "revision": "0c62aadb28ef57f8bcaf",
+    "url": "/portfolio/static/js/main.75b198d0.chunk.js"
+  },
+  {
+    "revision": "76aa8cbd67b5c937ec2c",
+    "url": "/portfolio/static/js/runtime-main.e92c632a.js"
+  },
+  {
+    "revision": "936d0f42192c2a500cd0599c2005de56",
+    "url": "/portfolio/static/media/cv.936d0f42.svg"
+  },
+  {
+    "revision": "af6134f2903eebdc17e57cd1193d040d",
+    "url": "/portfolio/static/media/email.af6134f2.svg"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/portfolio/static/media/github.76d344f0.svg"
+  },
+  {
+    "revision": "4e0c281b533dba047cb1fa2b3202f4cb",
+    "url": "/portfolio/static/media/smartphone.4e0c281b.svg"
+  }
+]);
