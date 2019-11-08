2021c6082b914c4d448660e31a681278970e8ce9
Vikintas Raudavicius <vikintas.raudavicius@gmail.com>
2019-11-08 14:20:52 +0000

    Updates

---
+++ precache-manifest.599920bb3587ef6ff37a0ac939f22d4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd47d5b51ad11f244631b51812cab08c",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "672ad253762388e5adf7",
+    "url": "/portfolio/static/css/main.70d361d0.chunk.css"
+  },
+  {
+    "revision": "5b31743322cf7d28fd6f",
+    "url": "/portfolio/static/js/2.5fcba992.chunk.js"
+  },
+  {
+    "revision": "672ad253762388e5adf7",
+    "url": "/portfolio/static/js/main.c16f18b4.chunk.js"
+  },
+  {
+    "revision": "76aa8cbd67b5c937ec2c",
+    "url": "/portfolio/static/js/runtime-main.e92c632a.js"
+  }
+]);
